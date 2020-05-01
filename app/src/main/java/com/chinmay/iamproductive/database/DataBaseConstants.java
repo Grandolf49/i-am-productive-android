147832b283cd3ab59bbf076dd8292b71f827420e
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-05-01 11:54:11 +0530

    project dao class completed

--- app/src/main/java/com/chinmay/iamproductive/database/DataBaseConstants.java
+++ app/src/main/java/com/chinmay/iamproductive/database/DataBaseConstants.java
@@ -4,7 +4,7 @@ package com.chinmay.iamproductive.database;
  * Created by grandolf49 on 13-04-2020
  */
 public class DataBaseConstants {
-    public static final String PROJECT_TABLE_NAME = "projects";
-    public static final String TASK_TABLE_NAME = "tasks";
-    public static final String RECORD_TABLE_NAME = "records";
+    public static final String PROJECT_TABLE_NAME = "project_table";
+    public static final String TASK_TABLE_NAME = "task_table";
+    public static final String RECORD_TABLE_NAME = "record_table";
 }
--- app/src/main/java/com/chinmay/iamproductive/database/dao/ProjectDao.java
+++ app/src/main/java/com/chinmay/iamproductive/database/dao/ProjectDao.java
@@ -1,7 +1,33 @@
 package com.chinmay.iamproductive.database.dao;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.chinmay.iamproductive.database.entities.ProjectEntity;
+import com.chinmay.iamproductive.database.entities.TaskEntity;
+
+import java.util.List;
+
 /**
  * Created by grandolf49 on 13-04-2020
  */
+@Dao
 public interface ProjectDao {
+    @Insert
+    void insert(ProjectEntity project);
+
+    @Update
+    void update(ProjectEntity project);
+
+    @Delete
+    void delete(ProjectEntity project);
+
+    @Query("select * from project_table")
+    List<ProjectEntity> getAllProjects();
+
+    @Query("select * from task_table where projectId=:id")
+    List<TaskEntity> getAllTasksByProjectId(int id);
 }
