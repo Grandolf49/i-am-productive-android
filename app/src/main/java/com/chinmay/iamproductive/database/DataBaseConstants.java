6df4a584c6aaaae08dc3f716a68b455c76af6365
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-05-01 12:17:31 +0530

    MainDatabase abstract class setup

--- app/src/main/java/com/chinmay/iamproductive/database/DataBaseConstants.java
+++ app/src/main/java/com/chinmay/iamproductive/database/DataBaseConstants.java
@@ -4,6 +4,7 @@ package com.chinmay.iamproductive.database;
  * Created by grandolf49 on 13-04-2020
  */
 public class DataBaseConstants {
+    public static final String PROJECT_DATABASE = "project_database";
     public static final String PROJECT_TABLE_NAME = "project_table";
     public static final String TASK_TABLE_NAME = "task_table";
     public static final String RECORD_TABLE_NAME = "record_table";
---
+++ app/src/main/java/com/chinmay/iamproductive/database/MainDatabase.java
@@ -0,0 +1,41 @@
+package com.chinmay.iamproductive.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.chinmay.iamproductive.database.dao.ProjectDao;
+import com.chinmay.iamproductive.database.dao.RecordDao;
+import com.chinmay.iamproductive.database.dao.TaskDao;
+import com.chinmay.iamproductive.database.entities.ProjectEntity;
+import com.chinmay.iamproductive.database.entities.RecordEntity;
+import com.chinmay.iamproductive.database.entities.TaskEntity;
+
+import static com.chinmay.iamproductive.database.DataBaseConstants.PROJECT_DATABASE;
+
+/**
+ * Created by grandolf49 on 01-05-2020
+ */
+@Database(entities = {ProjectEntity.class, TaskEntity.class, RecordEntity.class}, version = 1)
+public abstract class MainDatabase extends RoomDatabase {
+
+    private static MainDatabase instance;
+
+    public static synchronized MainDatabase getInstance(Context context) {
+        if (instance == null) {
+            return Room.databaseBuilder(context.getApplicationContext(),
+                    MainDatabase.class, PROJECT_DATABASE)
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+
+    public abstract ProjectDao projectDao();
+
+    public abstract TaskDao taskDao();
+
+    public abstract RecordDao recordDao();
+}
